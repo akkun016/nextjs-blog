f28010c8eedbcdd006cbfd150f291640c1c1fb3c
akkun016 <nakataakito@icloud.com>
2023-10-04 14:17:50 +0900

    レッスン５
    API Routesに関して（少しだけ記述）

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,6 @@ import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  }
-}
-
 export default function Home({allPostsData}) {
   console.log(allPostsData);
   return (
@@ -45,3 +36,13 @@ export default function Home({allPostsData}) {
     </Layout>
   );
 }
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  }
+}
+
